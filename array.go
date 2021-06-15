fe804b4954d1aaa34ca100414bd2c8b02a762db7
Ahmad Zakaria <85970382+AhmadZaka123@users.noreply.github.com>
2021-06-16 06:24:37 +0700

    Add files via upload

---
+++ array.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const = 1000
+
+type arrbilangan int
+
+
+func main (arr *arrbilangan, bil int ) int 
+    
---
+++ bst_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	allocate(P)
+	address root
+	insertNode(root,n,x)
+	PrintInorder(root,n,x)
+	
+	return 0;
+}
